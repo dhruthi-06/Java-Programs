d72b770a25456254773987d2a00755e942823dcb
dhruthi-06 <117371782+dhruthi-06@users.noreply.github.com>
2023-02-12 14:28:59 +0530

    Create  direct printing Using toString.java

---
+++ direct printing Using toString.java
@@ -0,0 +1,23 @@
+public class Student1 {
+ public int age;
+ public String name;
+ public int id;
+ public Student1(int id, String name, int age) {
+ this.id = id;
+ this.age = age;
+ this.name = name;
+ }
+ public String toString() {
+ return id + " " + name + " " + age;
+ }
+}
+public class Student {
+ public int age;
+ public String name;
+ public int id;
+ public Student(int id, String name, int age) {
+ this.id = id;
+ this.age = age;
+ this.name = name;
+ }
+}
